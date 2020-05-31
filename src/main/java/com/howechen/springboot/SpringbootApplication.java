8d53c8a770e9466f09e519b6824278fc30918f8d
HoweChen <yuhao.howe.chen@gmail.com>
2020-05-31 17:46:39 +0800

    update to support cache

--- src/main/java/com/howechen/springboot/SpringbootApplication.java
+++ src/main/java/com/howechen/springboot/SpringbootApplication.java
@@ -4,11 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @MapperScan("com.howechen.springboot.dao.mybatis")
+@EnableCaching
+@EnableScheduling
 public class SpringbootApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/howechen/springboot/dao/UserRepo.java
+++ src/main/java/com/howechen/springboot/dao/UserRepo.java
@@ -2,8 +2,10 @@ package com.howechen.springboot.dao;
 
 import com.howechen.springboot.entity.UserEntity;
 import java.util.Optional;
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.repository.CrudRepository;
 
+@RedisHash(value = "user")
 public interface UserRepo extends CrudRepository<UserEntity, String> {
 
   Optional<UserEntity> findByUsername(String username);

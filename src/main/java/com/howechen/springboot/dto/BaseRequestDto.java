c018d62689473ec99f02a0b6633d9379e69898ff
HoweChen <yuhao.howe.chen@gmail.com>
2020-06-19 23:03:05 +0800

    update BaseRequestDto

--- src/main/java/com/howechen/springboot/dto/BaseRequestDto.java
+++ src/main/java/com/howechen/springboot/dto/BaseRequestDto.java
@@ -2,12 +2,7 @@ package com.howechen.springboot.dto;
 
 import com.howechen.springboot.entity.BaseEntity;
 import com.howechen.springboot.support.DaoDtoBridge;
-import com.howechen.springboot.support.DtoDaoBridge;
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 /**
  * @author howechen
